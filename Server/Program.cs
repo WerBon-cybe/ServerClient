7217bb0ea817fdf60b12c88358284a5bfb2f79c5
annaz <annaz@ADMIN>
2024-01-17 22:12:34 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 13000);
+        server.Start();
+        Console.WriteLine("Сервер запущен. Ожидание подключения...");
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Подключен клиент: {0}", client.Client.RemoteEndPoint.ToString());
+            NetworkStream stream = client.GetStream();
+            string currentTime = DateTime.Now.ToString();
+            byte[] data = Encoding.ASCII.GetBytes(currentTime);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine("Получено: {0}", currentTime);
+            client.Close();
+        }
+    }
+}
---
+++ ServerClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient();
+                Console.Write("Введите IP-адрес удаленной машины: ");
+                string server = Console.ReadLine();
+                Console.Write("Введите порт: ");
+                int port = Convert.ToInt32(Console.ReadLine());
+                client.Connect(server, port);
+
+                Console.WriteLine("Подключение к {0} успешно установлено.", client.Client.RemoteEndPoint.ToString());
+                NetworkStream stream = client.GetStream();
+                byte[] data = System.Text.Encoding.ASCII.GetBytes("GetTime");
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Отправлено: GetTime");
+                data = new byte[256];
+                string responseData = string.Empty;
+                int bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Отправлено: {0}", responseData);
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Аргументы не могут быть null: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Ошибка сокета: {0}", e);
+            }
+
+            Console.WriteLine("\nНажмите Enter для выхода...");
+            Console.Read();
+        }
+    }
+
+
+}
